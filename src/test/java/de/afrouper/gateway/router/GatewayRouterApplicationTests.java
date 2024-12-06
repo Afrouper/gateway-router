fa2f32d575050e303b23167c1effcc2f7a864827
christian <dev@bestof5.de>
2024-12-06 07:49:45 +0100

    First try

--- src/test/java/de/afrouper/gateway/router/GatewayRouterApplicationTests.java
+++ src/test/java/de/afrouper/gateway/router/GatewayRouterApplicationTests.java
@@ -1,5 +1,10 @@
 package de.afrouper.gateway.router;
 
+import org.testcontainers.consul.ConsulContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -8,10 +13,20 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
+@Testcontainers
 class GatewayRouterApplicationTests {
+
+    @Container
+    static ConsulContainer consulContainer = new ConsulContainer("consul:latest");
 
     @Autowired
     private WebTestClient webTestClient;
+
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("spring.cloud.consul.host", consulContainer.getHost());
+        System.setProperty("spring.cloud.consul.port", String.valueOf(consulContainer.getFirstMappedPort()));
+    }
 
     @Test
     public void testTenantIdRouting() {
